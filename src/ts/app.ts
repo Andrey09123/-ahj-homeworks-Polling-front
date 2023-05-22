77a66943be4d87b374aca84830c8fa11184de8ce
Andre Arkhipov <andre.arhipow@gmail.com>
2023-05-22 16:38:45 +0300

    commit

---
+++ src/ts/app.ts
@@ -0,0 +1,7 @@
+import Widget from "./Widget";
+
+/* eslint-disable */
+console.log('it works!');
+
+new Widget(document.getElementById('polling-container'), 'https://polling-back.netlify.app/messages/unread');
+
